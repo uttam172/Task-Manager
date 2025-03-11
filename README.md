0d0a3b7ceb2809a9cf257443c924ecf6c8f45a73
uttam172 <uttamdobariya17@gmail.com>
2025-03-11 16:40:07 +0530

    Angular Project Setup with TailwindCSS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LearningAngular
+# TaskManager
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Learning-Angular' title`, () => {
+  it(`should have the 'task-manager' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Learning-Angular');
+    expect(app.title).toEqual('task-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Learning-Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task-manager');
   });
 });
