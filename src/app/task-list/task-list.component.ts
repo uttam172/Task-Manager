c9cca76051fdc8fe9864bb207433967ef62e5fcd
uttam172 <uttamdobariya17@gmail.com>
2025-03-12 09:23:02 +0530

    saving task in local storage functioality complete

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,21 +1,54 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-task-list',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 
 export class TaskListComponent {
   newTask: string = ""
-  task = 'Learn Angular 19'
-  taskInputPlaceholder = "Enter a new task..."
-  isDisabled = true
+  tasks: { text: string; completed: boolean } [] = []
+  
+  constructor() {
+    this.loadTasks()
+  }
 
-  toggleInput() {
-    this.isDisabled =!this.isDisabled
+  // Save tasks to local Storage
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
+  }
+
+  // Load tasks from Local Storage
+  loadTasks() {
+    const savedTasks = localStorage.getItem('tasks')
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks)
+    }
+  }
+
+  // Add Tasks
+  addTask() {
+    if (this.newTask.trim() !== "") {
+      this.tasks.push({ text: this.newTask, completed: false })
+      this.newTask = ""
+      this.saveTasks()
+    }
+  }
+
+  // Remove Tasks
+  removeTask(index: number) {
+    this.tasks.splice(index, 1)
+    this.saveTasks()
+  }
+
+  // Toggle Task Completion
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed
+    this.saveTasks()
   }
 }
